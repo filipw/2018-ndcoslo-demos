c2446eaaa5a44f7a1f554e9c633ff6e3a612bb16
filipw <wojcieszyn@gmail.com>
2018-05-30 20:47:49 +0200

    main in Startup

--- src/CorePackages/Startup.cs
+++ src/CorePackages/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -24,6 +25,13 @@ namespace Demos
             });
 
             app.UseMvc();
+        }
+
+        public static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build().Run();
         }
     }
 }
